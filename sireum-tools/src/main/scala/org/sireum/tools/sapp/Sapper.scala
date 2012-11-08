7fccad04f9a789518e229b9ff1f696c877b66cd5
Robby <robby@k-state.edu>
2012-11-07 18:13:27 -0600

    Added symlink support for sapp.

--- sireum-tools/src/main/scala/org/sireum/tools/sapp/Sapper.scala
+++ sireum-tools/src/main/scala/org/sireum/tools/sapp/Sapper.scala
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2011-2012 Robby, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+
 package org.sireum.tools.sapp
 
 import org.sireum.option._
@@ -7,6 +15,9 @@ import scala.collection.mutable._
 import java.nio.file.Files
 import java.util.Properties
 
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
 object Sapper {
   def run(option : SapperMode) {
     new Sapper().execute(option)
@@ -17,6 +28,9 @@ object Sapper {
   }
 }
 
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
 class Sapper {
   val buffer = new Array[Byte](1000)
 
@@ -39,42 +53,46 @@ class Sapper {
     import scala.collection.JavaConversions._
     val fPath = f.toPath
     if (Files.isSymbolicLink(fPath)) {
-      System.err.println("Sapper does not support symbolic link: " + fPath)
-      System.err.flush
-      return
-    }
-
-    var mask = 0
-    for (p <- Files.getPosixFilePermissions(fPath)) {
-      mask = mask | (1 << p.ordinal)
-    }
-    filePermProp.put(fPath.toString, mask.toString)
-
-    if (f.isDirectory) {
-      val p = f.getPath
-      val path = if (p.endsWith("/")) p else p + "/"
-      val ze = new ZipEntry(path)
+      val bytes = Files.readSymbolicLink(fPath).toFile.getPath.toString.getBytes
+      val ze = new ZipEntry(new File(f.getParentFile, f.getName + ".sapp_link").getPath)
       ze.setTime(f.lastModified)
+      ze.setSize(bytes.length)
       zos.putNextEntry(ze)
+      zos.write(bytes)
       zos.closeEntry
-      for (f2 <- f.listFiles) {
-        sapp(zos, f2, filePermProp)
-      }
     } else {
-      val fis = new FileInputStream(f)
-      try {
-        val path = f.getPath
+      var mask = 0
+      for (p <- Files.getPosixFilePermissions(fPath)) {
+        mask = mask | (1 << p.ordinal)
+      }
+      filePermProp.put(fPath.toString, mask.toString)
+
+      if (f.isDirectory) {
+        val p = f.getPath
+        val path = if (p.endsWith("/")) p else p + "/"
         val ze = new ZipEntry(path)
         ze.setTime(f.lastModified)
         zos.putNextEntry(ze)
+        zos.closeEntry
+        for (f2 <- f.listFiles) {
+          sapp(zos, f2, filePermProp)
+        }
+      } else {
+        val fis = new FileInputStream(f)
         try {
-          var n = fis.read(buffer)
-          while (n >= 0) {
-            zos.write(buffer, 0, n)
-            n = fis.read(buffer)
-          }
-        } finally zos.closeEntry
-      } finally fis.close
+          val path = f.getPath
+          val ze = new ZipEntry(path)
+          ze.setTime(f.lastModified)
+          zos.putNextEntry(ze)
+          try {
+            var n = fis.read(buffer)
+            while (n >= 0) {
+              zos.write(buffer, 0, n)
+              n = fis.read(buffer)
+            }
+          } finally zos.closeEntry
+        } finally fis.close
+      }
     }
   }
 }
