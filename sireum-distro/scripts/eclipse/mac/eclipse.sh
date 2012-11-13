e822a1344d924ace8dff3d121c8ca2a9d6bca0e9
Robby <robby@k-state.edu>
2012-11-13 16:17:24 -0600

    Updated eclipse scripts to pass arguments, if any. Conflicts: 	sireum-distro/scripts/eclipse/mac/eclipse.sh

--- sireum-distro/scripts/eclipse/mac/eclipse.sh
+++ sireum-distro/scripts/eclipse/mac/eclipse.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export SIREUM_HOME=`dirname $(cd "${0%/*}/../../../../../../" 2>/dev/null; echo "$PWD"/"${0##*/}")`
-$SIREUM_HOME/apps/eclipse/classic/Eclipse.app/Contents/MacOS/eclipse
+$SIREUM_HOME/apps/eclipse/classic/Eclipse.app/Contents/MacOS/eclipse "$@"
