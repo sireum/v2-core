048fc5593bdfb9e5dc2685c73a8597decaa9a87b
Robby <robby@k-state.edu>
2012-11-28 10:10:14 +0000

    Added Kiasan boolean extension.

---
+++ sireum-kiasan/src/main/scala/org/sireum/kiasan/extension/KiasanBooleanExtension.scala
@@ -0,0 +1,138 @@
+/*
+Copyright (c) 2011-2012 Robby, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+
+package org.sireum.konkrit.extension
+
+import org.sireum.extension._
+import org.sireum.extension.BooleanExtension._
+import org.sireum.extension.annotation._
+import org.sireum.kiasan.extension.annotation._
+import org.sireum.kiasan.extension.KiasanExtension._
+import org.sireum.kiasan.state._
+import org.sireum.pilar.ast._
+import org.sireum.pilar.eval._
+import org.sireum.pilar.state._
+import org.sireum.topi.annotation._
+import org.sireum.topi.process._
+import org.sireum.util._
+import org.sireum.util.math._
+
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
+object KiasanBooleanExtension extends ExtensionCompanion {
+  def create[S <: KiasanStatePart[S]](
+    config : EvaluatorConfiguration[S, Value, ISeq[(S, Value)], ISeq[(S, Boolean)], ISeq[S]]) =
+    new KiasanBooleanExtension(config)
+
+  val Type = "pilar://typeext/" + UriUtil.classUri(this) + "/Type"
+
+  @inline
+  def fresh[S <: KiasanStatePart[S]](s : S) = {
+    val (nextS, num) = s.next(KiasanBooleanExtension.Type)
+    (nextS, KB(num))
+  }
+
+
+  @BackEnd(value = "Z3", mode = "Process")
+  def z3BackEndPart = new TopiProcess.BackEndPart {
+    
+    class Context(var tc : TopiProcess.TypeCounters,
+                  val sb : StringBuilder = new StringBuilder) {
+      def result = (tc, sb.toString)
+    }
+
+    val lineSep = System.getProperty("line.separator")
+
+    @inline
+    implicit def i2s(i : Int) = i.toString
+
+    @inline
+    def println(ss : String*)(implicit ctx : Context) {
+      for (s <- ss)
+        ctx.sb.append(s)
+      ctx.sb.append(lineSep)
+    }
+
+    def declareConst(num : Int)(implicit ctx : Context) {
+      val lastNum = ctx.tc.getOrElse(KiasanBooleanExtension.Type, -1)
+      if (num > lastNum) {
+        for (i <- lastNum + 1 to num)
+          println("(declare-const b!", i, " Bool)")
+        ctx.tc = ctx.tc + (KiasanBooleanExtension.Type -> num)
+      }
+    }
+
+    def expTranslator = {
+      case (tc, ValueExp(KB(num))) =>
+        implicit val ctx = new Context(tc)
+        declareConst(num)
+        println("(assert b!", num, ")")
+        ctx.result
+      case (tc, UnaryExp("!", ValueExp(KB(num)))) =>
+        implicit val ctx = new Context(tc)
+        declareConst(num)
+        println("(assert (not b!", num, "))")
+        ctx.result
+    }
+
+    def stateRewriter(m : IMap[String, Value]) = {
+      case v @ KB(num) => m.getOrElse("b!" + num, v)
+    }
+  }
+}
+
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
+trait KiasanBooleanValue extends BooleanValue with KiasanValue
+
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
+final case class KB(num : Int) extends KiasanBooleanValue {
+  val typeUri = KiasanBooleanExtension.Type
+}
+
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
+final class KiasanBooleanExtension[S <: KiasanStatePart[S]](
+  config : EvaluatorConfiguration[S, Value, ISeq[(S, Value)], ISeq[(S, Boolean)], ISeq[S]])
+    extends Extension[S, Value, ISeq[(S, Value)], ISeq[(S, Boolean)], ISeq[S]] {
+
+  import KiasanBooleanExtension._
+
+  val uriPath = UriUtil.classUri(this)
+
+  type K = KiasanBooleanValue
+
+  @inline
+  private implicit def re2r(p : (S, Value)) = ilist(p)
+
+  @Cast
+  def castType : (S, Value, ResourceUri) --> ISeq[(S, Value)] = {
+    case (s, v : K, BooleanExtension.Type)       => (s, v)
+    case (s, v : K, KiasanBooleanExtension.Type) => (s, v)
+  }
+
+  val se = config.semanticsExtension
+
+  @Cond
+  def cond : (S, Value) --> ISeq[(S, Boolean)] = {
+    case (s, b : K) =>
+      ilist((s.addPathCondition(ValueExp(b)).requestInconsistencyCheck, true),
+        (s.addPathCondition(UnaryExp("!", ValueExp(b))).
+          requestInconsistencyCheck, false))
+  }
+
+  @FreshKiasanValue
+  def freshKB : (S, ResourceUri) --> (S, Value) = {
+    case (s, KiasanBooleanExtension.Type) => fresh(s)
+  }
+}
