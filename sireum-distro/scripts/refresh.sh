00b4efd89d6186c78f02d8bce6699f7162145ee1
Robby <robby@k-state.edu>
2012-12-04 16:12:32 -0600

    Removed obsoelete file copy.

--- sireum-distro/scripts/refresh.sh
+++ sireum-distro/scripts/refresh.sh
@@ -2,7 +2,3 @@ cat sireum-header ../src/main/scala/SireumDistro.scala > sireum
 cat sireum-header.bat ../src/main/scala/SireumDistro.scala > sireum.bat
 /opt/local/bin/unix2dos sireum-header.bat
 /opt/local/bin/unix2dos sireum.bat
-[ ! -e ../test ] && mkdir ../test
-cp sireum ../test/
-cp sireum.bat ../test/
-cp ../src/main/scala/SireumDistro.scala ../../../sireum-internal/sireumv2/project/
