c9f5b796c670143b7d77115b34584ec45af06a33
belt <belt@ksu.edu>
2012-12-11 13:34:40 -0600

    adding extractor gen

---
+++ sireum-bakar-xml/src/main/scala/org/sireum/bakar/xml/extractor/ExtractorGenerator.scala
@@ -0,0 +1,93 @@
+package org.sireum.bakar.xml.extractor
+
+import org.sireum.util._
+import java.io.File
+import org.sireum.bakar.xml._
+import org.stringtemplate.v4.ST
+import org.stringtemplate.v4.STGroupFile
+import java.io.FileWriter
+
+class ExtractorGenerator(outdir : File) {
+  val seen = msetEmpty[java.lang.Class[_]]
+
+  val stg : STGroupFile = new STGroupFile(getClass.getResource("extractor.stg"), "UTF-8", '$', '$')
+  val stTopLevel = stg.getInstanceOf("topLevel")
+
+  def process(c : java.lang.Class[_]) {
+    if(c.getEnclosingClass() != null){
+      println("ignoring enclosed class " + c.getName())
+      return
+    }
+    if (seen.contains(c))
+      return
+    else
+      seen += c
+
+    var oname = c.getSimpleName()
+    var fqname = c.getName()
+    if(c.getEnclosingClass() != null){
+      fqname = c.getEnclosingClass().getName() + "." + c.getSimpleName()
+      oname = c.getEnclosingClass().getSimpleName() + "_" + oname
+    }
+    
+    val stmain = stg.getInstanceOf("extractor")
+
+    stmain.add("name", oname)
+    stmain.add("fqname", fqname)
+
+    c.getDeclaredFields().foreach { f =>
+      val name = f.getName()
+      val annotations = f.getAnnotations()
+
+      val fname = f.getName()
+      val fmname = "get" + fname.charAt(0).toUpper + fname.drop(1) + "()"
+
+      val stfield = stg.getInstanceOf("field")
+      stfield.add("name", fmname)
+      stmain.add("field", stfield)
+
+      if (f.getType().getPackage() == c.getPackage()) {
+        process(f.getType)
+      }
+      else if (f.getType == classOf[java.lang.Object]) {
+        // fname is type which is reserved so we can't call it directly.  Instead
+        // use reflection
+        f.getDeclaredAnnotations().foreach { a =>
+          if (a.annotationType() == classOf[javax.xml.bind.annotation.XmlElements]) {
+            val xelems = a.asInstanceOf[javax.xml.bind.annotation.XmlElements]
+            xelems.value().foreach { elem =>
+              elem.getClass().getDeclaredMethods().foreach(f =>
+                if (f.getName == "type") {
+                  val ftype = f.invoke(elem).asInstanceOf[java.lang.Class[_]]
+                  if(ftype.getPackage() == c.getPackage){
+                    process(ftype)
+                  }
+                }
+              )
+            }
+          }
+        }
+      }
+    }
+
+    stTopLevel.add("entry", stmain)
+  }
+}
+
+object x {
+
+  def main(args : Array[String]) {
+    val root = classOf[CompilationUnit]
+    val outdir = new File(this.getClass().getResource(".").getPath().replace("bin", "src/main/scala"))
+
+    println(root)
+    println(outdir)
+
+    val eg = new ExtractorGenerator(outdir)
+    eg.process(root)
+    
+    val fw = new FileWriter(new File(outdir, "e.txt"))
+    fw.write(eg.stTopLevel.render);
+    fw.close
+  }
+}
