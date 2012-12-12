08fa7d773a38a330b281223febc6aaf238bb0160
belt <belt@ksu.edu>
2012-12-12 16:10:04 -0600

    tweaks

--- sireum-bakar-xml/src/main/scala/org/sireum/bakar/xml/module/BakarVisitorImpl.scala
+++ sireum-bakar-xml/src/main/scala/org/sireum/bakar/xml/module/BakarVisitorImpl.scala
@@ -5,26 +5,45 @@ import org.sireum.pipeline.PipelineJobModuleInfo
 import org.sireum.util.VisitorFunction
 import org.sireum.bakar.xml.extractor._
 import org.sireum.util.Visitor
+import org.sireum.util.PartialFunctionUtil
 
 class BakarVisitorDef(val job : PipelineJob, info : PipelineJobModuleInfo) extends BakarVisitorModule {
 
-  val f : VisitorFunction = {
+  type Visitor = Any => Boolean
+
+  trait Context {
+    var result : Object = null;
+
+    def pushResult(o : Object) {
+      //assert(result == null);
+      result = o
+    }
+
+    def popResult = {
+      //assert(result != null)
+      val t = result
+      result = null
+      t
+    }
+  }
+
+  def f(ctx : Context, v : => Visitor) : VisitorFunction = {
     case c @ CompilationUnitEx(a1, a2, a3, a4, a5, a6, a7, unitFullName, a9, a10) =>
       println("Compilation Unit " + unitFullName)
       true
-    case ProcedureBodyDeclarationEx(a1,a2,a3,names,a5,a,a7,a8,a9) =>
-        println("ProcedureDeclaration " + names.getDefiningNames())
-      true
+    case ProcedureBodyDeclarationEx(a1, a2, a3, names, a5, a, a7, a8, a9) =>
+      v(names)
+      println("ProcedureDeclaration " + ctx.popResult)
+      false
     case DefiningIdentifierEx(sloc, defname, defx, typex) =>
-      println("DefiningIdentifier " + defname)
+      ctx.pushResult(defname)
       true
   }
 
-  val results = this.parseGnat2XMLresults
+  def visitor : Visitor = b
+  val b = Visitor.build(PartialFunctionUtil.orElses(Seq(f(new Context {}, visitor))))
 
-  val b = Visitor.build(f)
-
-  results.foreach {
+  this.parseGnat2XMLresults.foreach {
     case (key, value) => {
       b(value)
     }
