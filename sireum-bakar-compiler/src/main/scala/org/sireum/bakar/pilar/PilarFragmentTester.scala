ecd43ef319bdf3a0deeefee45207eab3fd4ca383
Robby <robby@k-state.edu>
2012-12-12 16:38:02 -0600

    Fixed compile error.

--- sireum-bakar-compiler/src/main/scala/org/sireum/bakar/pilar/PilarFragmentTester.scala
+++ sireum-bakar-compiler/src/main/scala/org/sireum/bakar/pilar/PilarFragmentTester.scala
@@ -4,8 +4,8 @@ import org.sireum.pilar.parser.PilarParser
 import org.sireum.util.FileUtil
 
 object PilarFragmentTester {
-  val claz = org.sireum.pilar.ast.Exp.getClass
-  //val claz = org.sireum.pilar.ast.Model.getClass
+  val claz = classOf[org.sireum.pilar.ast.Exp]
+  //val claz = classOf[org.sireum.pilar.ast.Model.getClass]
   
   def main(args : Array[String]) {
     val frag = FileUtil.fileUri(this.getClass, "frag.plr")
