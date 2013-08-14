f58fde695cd4fb6ce597fbb6dd3d69d66b101d8a
Jason Belt <belt@ksu.edu>
2013-08-14 18:38:48 -0500

    revised bakar mode

--- sireum-option/src/main/scala/org/sireum/option/KiasanBakar.scala
+++ sireum-option/src/main/scala/org/sireum/option/KiasanBakar.scala
@@ -8,11 +8,60 @@ http://www.eclipse.org/legal/epl-v10.html
 
 package org.sireum.option
 
+import org.sireum.util._
+
 /**
  * @author <a href="mailto:robby@k-state.edu">Robby</a>
  */
-@Mode(command = "bakar", header = """
-Sireum/Kiasan for Spark
-(c) 2012, SAnToS Laboratory, Kansas State University
-""")
-case class KiasanBakarMode()
+@Main(value = "kiasan", 
+  //className = "org.sireum.bakar.tools.BakarKiasan",
+  //featureName = "Sireum Bakar Tools:Gnat.sapp",
+  className = "org.sireum.bakar.kiasan.driver.KiasanBakarDriver",
+  featureName = "Sireum Bakar Kiasan:Gnat.sapp:Z3.sapp",
+  desc = "Symbolic Execution of SPARK/Ada Programs")
+case class KiasanBakarMode(
+  general : KiasanBakarGeneralGroup = KiasanBakarGeneralGroup(),
+  analysis : KiasanBakarAnalysisGroup = KiasanBakarAnalysisGroup(),
+  report : KiasanBakarReportGroup = KiasanBakarReportGroup(),
+  //solver : KiasanBakarSolverGroup = KiasanBakarSolverGroup(),
+  
+  @Arg(index = 0, value="package-name")
+  var packageName : String = "",
+  
+  @Arg(index = 1, value="method-name")
+  var methodName : String = ""
+)
+
+@Group("General Options")
+case class KiasanBakarGeneralGroup(
+  @Option(longKey = "source-paths", desc = "Directories containing the SPARK source files")
+  var sourcePaths : ISeq[String] = ivectorEmpty,
+  
+  @Option(longKey = "source-files", desc = "Spark filenames")
+  var sourceFiles : ISeq[String] = ivectorEmpty
+)
+
+@Group("Analysis Options")
+case class KiasanBakarAnalysisGroup(
+  @Option(shortKey = "a", longKey = "array-bound", desc = "Array Element Bound")
+  var arrayBound : Int = 3,
+  
+  @Option(shortKey = "l", longKey = "loop-bound", desc = "Loop Bound")
+  var loopBound : Int = 10,
+  
+  @Option(shortKey = "i", longKey = "invoke-bound", desc = "Method invoke-chain bound")
+  var invokeBound : Int = 10,
+  
+  @Option(shortKey = "t", longKey = "timeout", desc = "Timeout in minutes")
+  var timeout : Int = 10,
+  
+  @Option(shortKey = "o", longKey = "outdir", desc = "Output directory path")
+  var outdir : String = "."
+)
+
+@Group("Report Options")
+case class KiasanBakarReportGroup()
+
+@Group("Solver Options")
+case class KiasanBakarSolverGroup()
+
