5c64c65eb5770bde5dd7a05d29bfcae72f318a27
jjedrys <jjedrys@santoslab.org>
2013-09-17 13:53:32 -0500

    osArch detection fix

--- sireum-cli/src/main/scala/org/sireum/cli/launcher/GpsLauncher.scala
+++ sireum-cli/src/main/scala/org/sireum/cli/launcher/GpsLauncher.scala
@@ -26,6 +26,7 @@ class GpsLauncher {
     gpsRelPath = gpsRelPath + "gps"
     val gps = new File(System.getenv("SIREUM_HOME"), gpsRelPath).getCanonicalPath()
     val e = new Exec
+    val osArch = OsArchUtil.detect
     val separator = if (osArch == OsArch.Win32 || osArch == OsArch.Win64) ";" else ":"
     println(separator)
     e.env("PATH") = System.getenv("SIREUM_HOME") + "apps/gnat/2014/bin/" + separator + System.getenv("PATH") 
