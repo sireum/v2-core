c5be04483e7329cf303504345bd8d3d2f33d765a
Robby <robby@k-state.edu>
2013-11-01 15:11:27 -0500

    Added a workaround for ANTLRWorks.app launching under OS X Mavericks by using antlrworks.jar directly.

--- sireum-cli/src/main/scala/org/sireum/cli/launcher/AntlrWorksLauncher.scala
+++ sireum-cli/src/main/scala/org/sireum/cli/launcher/AntlrWorksLauncher.scala
@@ -21,18 +21,17 @@ class AntlrWorksLauncher {
     var antlrRelPath = "apps/antlr/works/"
     OsArchUtil.detect match {
       case OsArch.Mac32 | OsArch.Mac64 =>
-        antlrRelPath = antlrRelPath + "ANTLRWorks.app"
+        antlrRelPath = antlrRelPath + "ANTLRWorks.app/Contents/Resources/Java/antlrworks.jar"
       case _ =>
         antlrRelPath = antlrRelPath + "antlrworks.jar"
     }
-    val antlrworks = new File(System.getenv("SIREUM_HOME"), antlrRelPath).getCanonicalPath()
+    val antlrworks = new File(System.getenv("SIREUM_HOME"), antlrRelPath).getCanonicalPath
     val e = new Exec
-    val args =
-      OsArchUtil.detect match {
-        case OsArch.Mac32 | OsArch.Mac64 =>
-          e.run(-1, ivector("open", antlrworks), None)
-        case _ =>
-          e.run(-1, ivector("java", "-jar", antlrworks), None)
-      }
+    OsArchUtil.detect match {
+      case OsArch.Mac32 | OsArch.Mac64 =>
+        e.run(-1, ivector("java", "-Xmx1024m", "-jar", antlrworks), None)
+      case _ =>
+        e.run(-1, ivector("java", "-jar", antlrworks), None)
+    }
   }
 }
