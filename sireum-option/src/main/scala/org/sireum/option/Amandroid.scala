3afc8d096e10a68726f80e3d3293f2c15eb7f489
Fengguo Wei <fengguowei@dhcp242-11.user.cis.ksu.edu>
2014-03-04 12:39:15 -0600

    added amandroid cli

--- sireum-option/src/main/scala/org/sireum/option/Amandroid.scala
+++ sireum-option/src/main/scala/org/sireum/option/Amandroid.scala
@@ -9,10 +9,13 @@ import org.sireum.util._
   desc = "Sireum Amandroid Tools",
   header = """
 Sireum for Android
-(c) 2012, SAnToS Laboratory, Kansas State University
+(c) 2014, SAnToS Laboratory, Kansas State University
 """)
 case class SireumAmandroidMode(
   decompile : SireumAmandroidDecompileMode = SireumAmandroidDecompileMode(),
+  passwordTracking : SireumAmandroidPasswordTrackingMode = SireumAmandroidPasswordTrackingMode(),
+  intentInjection : SireumAmandroidIntentInjectionMode = SireumAmandroidIntentInjectionMode(),
+  cryptoMisuse : SireumAmandroidCryptoMisuseMode = SireumAmandroidCryptoMisuseMode(),
   taintAnalysis : SireumAmandroidTaintAnalysisMode = SireumAmandroidTaintAnalysisMode())
 
 object DumpSource extends Enum {
@@ -45,21 +48,99 @@ object AnalyzeSource extends Enum {
 
   def elements = ivector(APK, DIR)
 }
+
+@Main(value = "passwordTracking",
+  className = "org.sireum.amandroid.security.cli.PasswordTrackingCli",
+  featureName = "Sireum Amandroid Alir",
+  desc = "Tracking password flow within Android app")
+case class SireumAmandroidPasswordTrackingMode(
+    
+  general : SireumAmandroidGeneralGroup = SireumAmandroidGeneralGroup(),
+  analysis : SireumAmandroidAnalysisGroup = SireumAmandroidAnalysisGroup(),
+    
+  @Arg(index = 0, value = "Source file")
+  var srcFile : String = "",
+    
+  @Arg(index = 1, value = "Sink list file")
+  var sasFile : String = ""
+  )
+  
+@Main(value = "intentInjection",
+  className = "org.sireum.amandroid.security.cli.IntentInjectionCli",
+  featureName = "Sireum Amandroid Alir",
+  desc = "Detecting Intent injection problem")
+case class SireumAmandroidIntentInjectionMode(
+    
+  general : SireumAmandroidGeneralGroup = SireumAmandroidGeneralGroup(),
+  analysis : SireumAmandroidAnalysisGroup = SireumAmandroidAnalysisGroup(),
+    
+  @Arg(index = 0, value = "Source file")
+  var srcFile : String = "",
+    
+  @Arg(index = 1, value = "Sink list file")
+  var sasFile : String = ""
+  )
+  
+@Main(value = "cryptoMisuse",
+  className = "org.sireum.amandroid.security.cli.CryptoMisuseCli",
+  featureName = "Sireum Amandroid Alir",
+  desc = "Detecting crypto API misuse")
+case class SireumAmandroidCryptoMisuseMode(
+    
+  general : SireumAmandroidGeneralGroup = SireumAmandroidGeneralGroup(),
+  analysis : SireumAmandroidAnalysisGroup = SireumAmandroidAnalysisGroup(),
+    
+  @Arg(index = 0, value = "Source file")
+  var srcFile : String = "",
+    
+  @Arg(index = 1, value = "Sink list file")
+  var sasFile : String = ""
+  )
   
 @Main(value = "taintAnalysis",
-  className = "org.sireum.amandroid.alir.TaintAnalyzeCli",
+  className = "org.sireum.amandroid.security.cli.TaintAnalyzeCli",
   featureName = "Sireum Amandroid Alir",
   desc = "Analyze Android apk and output the result")
 case class SireumAmandroidTaintAnalysisMode(
-  @Option(shortKey = "t", longKey = "type", desc = "The type of the file you want to Analyze.")
-  var typ : AnalyzeSource.Type = AnalyzeSource.APK,
-
+    
+  general : SireumAmandroidGeneralGroup = SireumAmandroidGeneralGroup(),
+  analysis : SireumAmandroidAnalysisGroup = SireumAmandroidAnalysisGroup(),
+  
   @Arg(index = 0, value = "Source file")
   var srcFile : String = "",
+    
+  @Arg(index = 1, value = "Sink list file")
+  var sasFile : String = "")
   
-  @Arg(index = 1, value = "Source&Sink list file")
-  var sasFile : String = "",
+@Group("General Options")
+case class SireumAmandroidGeneralGroup(
+  @Option(shortKey = "t", longKey = "type", desc = "The type of the file you want to analyze.")
+  var typ : AnalyzeSource.Type = AnalyzeSource.APK,
+  
+  @Option(shortKey = "m", longKey = "memory", desc = "Max memory (GB).")
+  var mem : Int = 2
+)
 
-  @OptionalArg(index = 2, value = "Output Dir")
-  var outFile : String = "")
+@Group("Analysis Options")
+case class SireumAmandroidAnalysisGroup(
+  @Option(shortKey = "ns", longKey = "nostatic", desc = "Does not handle static initializer")
+  var noStatic : Boolean = false,
   
+  @Option(shortKey = "par", longKey = "parallel", desc = "Parallel")
+  var parallel : Boolean = false,
+  
+  @Option(shortKey = "ni", longKey = "no-icc", desc = "Does not tracking flows via icc")
+  var noicc : Boolean = false,
+  
+  @Option(shortKey = "k", longKey = "k-context", desc = "Context length")
+  var k_context : Int = 1,
+  
+  @Option(shortKey = "t", longKey = "timeout", desc = "Timeout (minute)")
+  var timeout : Int = 10,
+  
+  @Option(shortKey = "o", longKey = "outdir", desc = "Output directory path")
+  var outdir : String = ".",
+    
+  @Option(shortKey = "ls", longKey = "libSideEffect", desc = "Lib side effect summary path")
+  var sideeffectPath : String = ""
+)
