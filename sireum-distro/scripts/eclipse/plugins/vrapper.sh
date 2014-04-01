a69ea3275c15b9444151601132374f64ff890b4f
Robby <robby@k-state.edu>
2014-04-01 18:58:46 -0500

    Moved vrapper out of SireumDev to its own Vim feature.

---
+++ sireum-distro/scripts/eclipse/plugins/vrapper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+export PACKAGE_HOME=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
+export VRAPPER_DROP_URL=${VRAPPER_URL:=http://sourceforge.net/projects/vrapper/files/vrapper/}${VRAPPER_VERSION:=0.40.0}/${VRAPPER_DROP:=vrapper_${VRAPPER_VERSION}_20140208.zip}
+if [ ! -f "$VRAPPER_DROP" ]; then
+  echo
+  echo Downloading Vrapper $VRAPPER_DROP
+  echo
+  wget $VRAPPER_DROP_URL
+  echo
+fi
+#
+# Vrapper
+#
+mkdir eclipse 2> /dev/null
+mkdir eclipse/dsl 2> /dev/null
+mkdir eclipse/dsl/links 2> /dev/null
+echo "path=../../vim/vrapper" > eclipse/dsl/links/vrapper.link 
+mkdir Vim 2> /dev/null
+mkdir Vim/vrapper 2> /dev/null
+mkdir Vim/vrapper/eclipse 2> /dev/null
+cd Vim/vrapper/eclipse
+unzip -oq ../../../$VRAPPER_DROP
+> .eclipseextension
+cd ../..
+zip -rq vrapper.sapp vrapper ../eclipse/dsl/links/vrapper.link
+cd ..
+rm -fR Vim/vrapper
+echo
+echo ...done!
