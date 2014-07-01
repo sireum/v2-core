1983c63673448e6bbfdf115779aada1cebc1d099
Robby <robby@k-state.edu>
2014-07-01 15:15:43 -0500

    Added ProgressManager utility interface.

---
+++ sireum-pilar/src/main/scala/org/sireum/pilar/util/ProgressManager.scala
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2014 Robby, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+
+package org.sireum.pilar.util
+
+/**
+ * @author <a href="mailto:robby@k-state.edu">Robby</a>
+ */
+trait ProgressManager {
+  def terminated : Boolean
+}
