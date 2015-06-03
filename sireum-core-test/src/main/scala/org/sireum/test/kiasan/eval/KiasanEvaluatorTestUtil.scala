b23305ac2db91bd54a11e8148d9eb47acc7690d3
Robby <robby@k-state.edu>
2015-06-02 23:40:14 -0500

    Moved Kiasan and Konkrit TestUtil to src/main/scala from src/main/test.

--- sireum-core-test/src/main/scala/org/sireum/test/kiasan/eval/KiasanEvaluatorTestUtil.scala
+++ sireum-core-test/src/main/scala/org/sireum/test/kiasan/eval/KiasanEvaluatorTestUtil.scala
@@ -1,24 +1,22 @@
 /*
-Copyright (c) 2011-2013 Robby, Kansas State University.        
-All rights reserved. This program and the accompanying materials      
-are made available under the terms of the Eclipse Public License v1.0 
-which accompanies this distribution, and is available at              
-http://www.eclipse.org/legal/epl-v10.html                             
+Copyright (c) 2011-2013 Robby, Kansas State University.
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html
 */
 
 package org.sireum.test.kiasan.eval
 
-import org.sireum.pilar.ast._
-import org.sireum.pilar.state._
-import org.sireum.kiasan.Kiasan._
-import org.sireum.kiasan.state._
-import org.sireum.kiasan.extension._
-import org.sireum.konkrit.extension._
-import org.sireum.util._
-import org.sireum.pilar.eval._
 import org.sireum.extension._
+import org.sireum.kiasan.Kiasan._
+import org.sireum.kiasan.extension._
+import org.sireum.pilar.ast._
+import org.sireum.pilar.eval._
+import org.sireum.pilar.state._
 import org.sireum.pilar.symbol._
 import org.sireum.test.util.pilar.eval.PilarEvalUtil._
+import org.sireum.util._
 
 /**
  * @author <a href="mailto:robby@k-state.edu">Robby</a>
--- sireum-core-test/src/main/scala/org/sireum/test/konkrit/eval/KonkritEvaluatorTestUtil.scala
+++ sireum-core-test/src/main/scala/org/sireum/test/konkrit/eval/KonkritEvaluatorTestUtil.scala
@@ -1,20 +1,20 @@
 /*
-Copyright (c) 2011-2013 Robby, Kansas State University.        
-All rights reserved. This program and the accompanying materials      
-are made available under the terms of the Eclipse Public License v1.0 
-which accompanies this distribution, and is available at              
-http://www.eclipse.org/legal/epl-v10.html                             
+Copyright (c) 2011-2013 Robby, Kansas State University.
+All rights reserved. This program and the accompanying materials
+are made available under the terms of the Eclipse Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/epl-v10.html
 */
 
 package org.sireum.test.konkrit.eval
 
-import org.sireum.pilar.state._
+import org.sireum.extension._
 import org.sireum.konkrit.extension._
 import org.sireum.pilar.eval._
-import org.sireum.util._
-import org.sireum.extension._
+import org.sireum.pilar.state._
 import org.sireum.pilar.symbol._
 import org.sireum.test.util.pilar.eval.PilarEvalUtil._
+import org.sireum.util._
 import org.sireum.util.math.SireumNumber
 
 /**
